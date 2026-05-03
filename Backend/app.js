a623d1cb47baf768134c5d93a41bb90ab12d5aa9
JhonatangarciaTrycore <jhonatan.garcia@trycore.com>
2026-05-02 20:09:42 -0500

    Implement project CRUD endpoints

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import { db } from './db.config.js';
 import cors from 'cors';
+import projectRoutes from './routes/project.routes.js';
 
 
 const app = express();
@@ -10,16 +11,16 @@ const PORT = Number(process.env.PORT || 3000);
 app.use(express.json());
 app.use(cors());
 
+
+app.use('/projects', projectRoutes);
+
 db.query('SELECT NOW()')
     .then(() => console.log('Database connected'))
     .catch((error) => console.error('Database connection error', error));
 
-
 app.listen(PORT, () => {
     console.log(`Servidor EVM escuchando en puerto ${PORT}`);
 });
-
-
 
 
 export { app };
